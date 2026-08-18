8aa3494d6930f371a59ffa10b1d4d0cb2aa83481
joshdholtz <401294+joshdholtz@users.noreply.github.com>
2026-08-18 03:45:08 +0000

    Bump version to 0.6.1

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ import {
 } from './storage';
 
 /** SDK version for metrics headers */
-const SDK_VERSION = '0.6.0';
+const SDK_VERSION = '0.6.1';
 
 export type { MGMConfiguration, EventProperties, UserProfile };
 
